42daaf36d802a19200fbc48a349ecdbccee2cd58
IlshatIbrahimov <ilshat.ibrahimov721@gmail.com>
2019-11-19 15:39:13 +0300

    Fix API

--- src/main/java/com/spacex/api/Flight.java
+++ src/main/java/com/spacex/api/Flight.java
@@ -1,7 +1,11 @@
 package com.spacex.api;
+
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -13,6 +17,7 @@ import static java.lang.String.format;
 
 public class Flight {
 
+    private String flightNumber;
     private String flightName;
     private String description;
     private String launchDate;
@@ -20,18 +25,27 @@ public class Flight {
     private String launchSuccess;
     private String wikipedia;
     private String videoLink;
+    private String missionPatchUrl;
+    private String imageUrl;
+
 
     public static Flight getFlight(String flightId){
         Flight flight = new Flight();
         flight.initInfo(flightId);
+        String[] images = getData(flightId).get("links").getAsJsonObject().get("flickr_images").toString().replaceAll("\\[","").replaceAll("\\]","").replaceAll("\"","").split(",");
+        flight.imageUrl = getImage(images);
         return flight;
+    }
+
+    public static Flight getFlight(int flightId){
+        return getFlight(String.valueOf(flightId));
     }
 
     public static List<Flight> getAllFlights(){
         List<Flight> allFlights = new ArrayList<>();
         for (int i = 1; i <= Integer.parseInt(getData("latest").get("flight_number").toString()); i++) {
-            Flight flight = new Flight();
-            flight.initInfo(String.valueOf(i));
+            Flight flight = getFlight(i);
+            flight.imageUrl = null;
             allFlights.add(flight);
         }
         return allFlights;
@@ -39,17 +53,16 @@ public class Flight {
 
     private void initInfo(String flightId){
         JsonObject data = getData(flightId);
-        flightName = data.get("mission_name").toString();
+        flightNumber = data.get("flight_number").toString();
+        flightName = data.get("mission_name").toString().replaceAll("\"","");
         description = data.get("details").toString();
-        launchDate = data.get("launch_date_local").toString();
-        rocketName = data.get("rocket").getAsJsonObject().get("rocket_name").toString();
+        description = description.substring(1, description.length()-1);
+        launchDate = data.get("launch_date_local").toString().replaceAll("\"","");
+        rocketName = data.get("rocket").getAsJsonObject().get("rocket_name").toString().replaceAll("\"","");
         launchSuccess = data.get("launch_success").toString();
-        wikipedia = data.get("links").getAsJsonObject().get("wikipedia").toString();
-        videoLink = data.get("links").getAsJsonObject().get("video_link").toString();
-    }
-
-    private void initInfo(int flightId){
-        initInfo(String.valueOf(flightId));
+        wikipedia = data.get("links").getAsJsonObject().get("wikipedia").toString().replaceAll("\"", "");
+        videoLink = data.get("links").getAsJsonObject().get("youtube_id").toString().replaceAll("\"","");
+        missionPatchUrl = data.get("links").getAsJsonObject().get("mission_patch_small").toString().replaceAll("\"","");
     }
 
     private static JsonObject getData(String dataId) {
@@ -67,6 +80,33 @@ public class Flight {
             e.printStackTrace();
         }
         return res;
+    }
+
+    private static String getImage(String[] images){
+        String image = "";
+        int minHeight = 0;
+        int minHeightId = 0;
+        try{
+            for (int i = 0; i < images.length; i++) {
+                URL url = new URL(images[i]);
+                final BufferedImage bi = ImageIO.read(url);
+                if (bi.getHeight() < minHeight){
+                    minHeight = bi.getHeight();
+                    minHeightId = i;
+                }
+                if (bi.getHeight() < bi.getWidth()){
+                    image = images[i];
+                    break;
+                }
+            }
+            if (image.isEmpty()){
+                image = images[minHeightId];
+            }
+        }
+        catch (IOException e){
+            image = "https://www.doz.com/cms/wp-content/uploads/2015/03/spacex-logo.jpg";
+        }
+        return image;
     }
 
 }
